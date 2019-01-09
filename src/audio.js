758a109177d7f2f0fb2de6c75e938e9e427f4b2d
Leisan Kazberova <leisan.kh@gmail.com>
2019-01-09 19:56:26 +0400

    Change definitions

--- src/audio.js
+++ src/audio.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 const Audio = require('audio-2.0.0');
-// const encode = require('audio-encode/mp3')
 const lamejs = require('lamejs');
+
 function encodeMono(channels, sampleRate, samples) {
-  var buffers = [];
-  var mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128 / 2);
-  var remaining = samples.length;
-  var maxSamples = 1152;
-  var length = 0;
-  for (var i = 0; remaining >= maxSamples; i += maxSamples) {
-    var mono = samples.subarray(i, i + maxSamples);
-    var mp3buf = mp3enc.encodeBuffer(mono);
+  let buffers = [];
+  const mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128 / 2);
+  let remaining = samples.length;
+  const maxSamples = 1152;
+  let length = 0;
+  for (let i = 0; remaining >= maxSamples; i += maxSamples) {
+    const mono = samples.subarray(i, i + maxSamples);
+    const mp3buf = mp3enc.encodeBuffer(mono);
     if (mp3buf.length > 0) {
       length += mp3buf.length;
       buffers.push(Buffer.from(mp3buf));
     }
     remaining -= maxSamples;
   }
-  var d = mp3enc.flush();
+  const d = mp3enc.flush();
   if (d.length > 0) {
     length += d.length;
     buffers.push(Buffer.from(d));
   }
-  var mp3data = Buffer.concat(buffers, length);
-  return mp3data;
+  return Buffer.concat(buffers, length);
 }
 module.exports.addGap = (file, gap) => {
   return new Promise((resolve, reject) =>
